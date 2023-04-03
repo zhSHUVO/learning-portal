b8146e218c9ef6d05c639d327e891937739a9e55
zhshuvo <zhshuvo99@gmail.com>
2023-04-04 01:54:42 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Nav from "./Components/Nav";
 import AdminLogin from "./pages/admin/AdminLogin";
+import Dashboard from "./pages/admin/Dashboard";
 
 function App() {
     return (
         <div>
+            <Nav />
             <Router>
                 <Routes>
                     <Route path="/admin" element={<AdminLogin />} />
+                    <Route path="/admin/dashboard" element={<Dashboard />} />
                 </Routes>
             </Router>
             <Toaster />
